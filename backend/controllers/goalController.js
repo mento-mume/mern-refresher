7a9b10dad71886a453e18846829369f6114fdfee
Mento Mume <akpankevman@gmail.com>
2022-07-08 06:59:07 +0100

    Goals controllers and routes completed

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,34 +1,45 @@
+const { errorHandler } = require("../middleware/errorMiddleware")
+
+const asyncHandler =require(express-async-handler)
+
 //@desc Get Goals
 //@route GET /api/goals
 //@access private
 
-const getGoals = (req, res) =>{
+const getGoals = asyncHandler(async (req, res) =>{
     res.status(200).json({message:'Get goals'})
-}
+})
 
 //@desc Set Goals
 //@route POST /api/goals
 //@access private
 
-const setGoal = (req, res) =>{
+const setGoal = asyncHandler(async (req, res) =>{
+
+if(!req.body.text)   
+    {
+         res.status(400)
+    throw new Error('please add text')
+    }
+
     res.status(200).json({message:'Set goals'})
-}
+})
 
 //@desc Update Goal
 //@route PUT /api/goals
 //@access private
 
-const updateGoal = (req, res) =>{
+const updateGoal = asyncHandler(async (req, res) =>{
     res.status(200).json({message:`Update goals ${req.params.id}`})
-}
+})
 
 //@desc Delete Goals
 //@route DELETE /api/goals
 //@access private
 
-const deleteGoal = (req, res) =>{
+const deleteGoal = asyncHandler(async (req, res) =>{
     res.status(200).json({message:`delete goals ${req.params.id}`})
-}
+})
 
 module.exports ={
     getGoals,

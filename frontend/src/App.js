19ea61c650ea50ebf0b630361e74836b062aed1a
Jacques Mac <jacques.ikot@foundersworkshop.com>
2022-07-31 17:43:33 +0100

    changes to code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,40 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from 'react-router-dom';
 
 import Header from './components/Header';
-import Dashboard  from './pages/Dashboard';
+import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
-
 function App() {
   return (
-    <>
-      <Router>
-        <div className="container">
-        <Header/>
-          <Routes>
-            <Route path='/' element ={<Dashboard/>}/>
-            <Route path='/login' element ={<Login/>}/>
-            <Route path='/register' element ={<Register/>}/>       
-          </Routes>          
-        </div>
-      </Router>    
-    </>
-  )
+    // <>
+    //   <Router>
+    //     <div className="container">
+    //       <Header />
+    //       <Routes>
+    //         <Route path="/" element={<Dashboard />} />
+    //         <Route path="/login" element={<Login />} />
+    //         <Route path="/register" element={<Register />} />
+    //       </Routes>
+    //     </div>
+    //   </Router>
+    // </>
+    <BrowserRouter>
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,26 +1,45 @@
-import {FaSignInAlt, FaUser} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import { FaSignInAlt, FaUser } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className = "header">
-    <div className="logo">
-        <Link to='/'>GoalSetter</Link>
-    </div>
-    <ul>
-      <li>
-        <link to='/login'>
-          <FaSignInAlt>Login</FaSignInAlt>
-        </link>
-      </li>
-      <li>
-        <link to='/register'>
-          <FaUser>Login</FaUser>
-        </link>
-      </li>
-    </ul>
+    // <header className="header">
+    //   <div className="logo">
+    //     <Link to="/">GoalSetter</Link>
+    //   </div>
+    // <ul>
+    //   <li>
+    //     <link to="/login">
+    //       <FaSignInAlt>Login</FaSignInAlt>
+    //     </link>
+    //   </li>
+    //   <li>
+    //     <link to="/register">
+    //       <FaUser>Login</FaUser>
+    //     </link>
+    //   </li>
+    // </ul>
+    // </header>
+    <header className="header">
+      <div className="logo">
+        <Link to="/">GoalSetter</Link>
+      </div>
+
+      <ul>
+        <li>
+          <Link to={'/login'}>
+            <FaSignInAlt>Login</FaSignInAlt>
+          </Link>
+        </li>
+
+        <li>
+          <Link to={'/register'}>
+            <FaUser>Login</FaUser>
+          </Link>
+        </li>
+      </ul>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
